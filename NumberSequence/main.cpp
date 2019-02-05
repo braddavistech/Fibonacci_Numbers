7d38a85acaf3ff5f48151a5290c880e665847f0d
braddavistech <brad@braddavistech.com>
2019-02-05 13:48:25 -0600

    Initial Commit

---
+++ NumberSequence/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NumberSequence
+//
+//  Created by Brad Davis on 2/5/19.
+//  Copyright © 2019 Brad Davis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
